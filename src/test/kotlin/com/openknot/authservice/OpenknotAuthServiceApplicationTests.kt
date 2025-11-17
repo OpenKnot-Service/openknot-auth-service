6bc72bea71f9ca265d50e9ea724f4c6c0f7ebf81
TaeHee00 <kancth03@gmail.com>
2025-11-17 21:22:13 +0900

    chore: setup

---
+++ src/test/kotlin/com/openknot/authservice/OpenknotAuthServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.openknot.authservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenknotAuthServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
