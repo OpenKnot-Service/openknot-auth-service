3b97dd1ec8e5cd75108a48f05aa49285e2d8b8fd
TaeHee00 <kancth03@gmail.com>
2025-11-17 21:43:47 +0900

    feat: JWT Token Properties 구현

---
+++ src/main/kotlin/com/openknot/auth/config/TokenProperties.kt
@@ -0,0 +1,10 @@
+package com.openknot.auth.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "jwt")
+data class TokenProperties(
+    val secret: String,
+    val accessTokenExpiration: Long,
+    val refreshTokenExpiration: Long
+)
